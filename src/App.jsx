495378870cfdbad1766bb417868bd08489860916
Jonathan Adami <pitilezard@gmail.com>
2021-05-19 08:27:58 +1000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+
+import { Paper, Grid, TextField, FormControl, InputLabel, Select, MenuItem, LinearProgress } from "@material-ui/core";
+import { Person, Alcohol, Drink } from "./lib";
+import { DateTime } from "luxon";
+
+const App = (props) => {
+    const [height, setHeight] = useState("183");
+    const [weight, setWeight] = useState("82");
+    const [gender, setGender] = useState("male");
+
+    const person = new Person(gender, height, weight);
+
+    const [volume, setVolume] = useState(500);
+    const [percentage, setPercentage] = useState(5.32);
+    const [standardDrinks, setStandardDrinks] = useState(2.1);
+
+    const alcohol1 = Alcohol.fromPercentageAndVolume(percentage, volume);
+    const alcohol2 = Alcohol.fromStandardDrink(standardDrinks);
+
+    const start_date = DateTime.local();
+    const drink = new Drink(start_date, 60 * 30, alcohol2);
+    const alcohol_data = new Array(61)
+        .fill()
+        .map((_, m) => drink.getAlcoholContentForDate(person, start_date.plus({ minutes: m })));
+
+    return (
+        <Grid container spacing={4}>
+            <Grid item xs={3}>
+                <Paper style={{ textAlign: "center", padding: "2em" }}>
+                    <TextField
+                        id="height"
+                        label="Height (in cm)"
+                        style={{ width: "100%" }}
+                        value={height}
+                        onChange={(ev) => setHeight(ev.target.value)}
+                    />
+                    <br />
+                    <TextField
+                        id="weight"
+                        label="Weight (in kg)"
+                        style={{ width: "100%" }}
+                        value={weight}
+                        onChange={(ev) => setWeight(ev.target.value)}
+                    />
+                    <br />
+                    <FormControl style={{ width: "100%" }}>
+                        <InputLabel>Gender</InputLabel>
+                        <Select value={gender} onChange={(ev) => setGender(ev.target.value)}>
+                            <MenuItem value="male">Male</MenuItem>
+                            <MenuItem value="female">Female</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Paper>
+                <Paper style={{ textAlign: "center", padding: "2em" }}>
+                    <h2>BMI:</h2>
+                    <pre>{person.getBMI()}</pre>
+                    <hr />
+                    <h2>Blood Volume:</h2>
+                    <dl style={{ textAlign: "left" }}>
+                        <dt>Nadler</dt>
+                        <dd>{person.getNadlerBloodVolume()} ml</dd>
+
+                        <dt>Lemmens-Bernstein-Brodsky</dt>
+                        <dd>{person.getLemmensBernsteinBrodskyBloodVolume()} ml</dd>
+
+                        <dt>Weight</dt>
+                        <dd>{person.getWeightBasedBloodVolume()} ml</dd>
+
+                        <dt>Average</dt>
+                        <dd>{person.getBloodVolume()} ml</dd>
+                    </dl>
+                </Paper>
+            </Grid>
+            <Grid item xs={3}>
+                <Paper style={{ textAlign: "center", padding: "2em" }}>
+                    <h2>Alcohol1:</h2>
+                    <p>(volume, %age)</p>
+                    <TextField
+                        id="volume"
+                        label="Volume (in ml)"
+                        style={{ width: "100%" }}
+                        value={volume}
+                        onChange={(ev) => setVolume(ev.target.value)}
+                    />
+                    <br />
+                    <TextField
+                        id="percentage"
+                        label="Percentage of Alcohol"
+                        style={{ width: "100%" }}
+                        value={percentage}
+                        onChange={(ev) => setPercentage(ev.target.value)}
+                    />
+                    <br />
+                    <h3>{alcohol1.content} mg</h3>
+                    <h4>BAC: {person.getBACInGramPct(alcohol1)} gr/100ml</h4>
+                    <hr />
+                    <h2>Alcohol2:</h2>
+                    <p>(standard drinks)</p>
+                    <TextField
+                        id="standardDrinks"
+                        label="Standard Drinks"
+                        style={{ width: "100%" }}
+                        value={standardDrinks}
+                        onChange={(ev) => setStandardDrinks(ev.target.value)}
+                    />
+                    <br />
+                    <h3>{alcohol2.content} mg</h3>
+                    <h4>BAC: {person.getBACInGramPct(alcohol2)} gr/100ml</h4>
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper style={{ textAlign: "center", padding: "2em" }}>
+                    <h2>BAC over time</h2>
+                    <p>For alcohol2</p>
+                    {alcohol_data.map((v, i) => (
+                        <Grid container key={i} style={{ height: "1em" }}>
+                            <Grid item xs={1}>
+                                {i % 5 === 0 && <span>{i}mn</span>}
+                            </Grid>
+                            <Grid item xs={11}>
+                                <LinearProgress variant="determinate" value={v * 10} style={{ height: "1em" }} />
+                            </Grid>
+                        </Grid>
+                    ))}
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/lib.jsx
@@ -0,0 +1,86 @@
+import { DateTime } from "luxon";
+
+const VOL2WEIGHT = 1.267;
+const ABSORPTION_RATE = 0.5;
+const ELIMINATION_RATE = 0.4;
+const BIOAVAILABILITY = 0.8;
+
+export class Person {
+    constructor(gender, height, weight) {
+        this.gender = gender; // male/female
+        this.height = height; // in cm
+        this.weight = weight; // in kg
+    }
+
+    getBloodVolume() {
+        return (
+            (this.getNadlerBloodVolume() +
+                this.getLemmensBernsteinBrodskyBloodVolume() +
+                this.getWeightBasedBloodVolume()) /
+            3
+        );
+    }
+
+    getNadlerBloodVolume() {
+        const factors = this.gender == "male" ? [0.3669, 0.03219, 0.6041] : [0.3561, 0.03308, 0.1833];
+        return (factors[0] * Math.pow(this.height / 100, 3) + factors[1] * this.weight + factors[2]) * 1000;
+    }
+
+    getLemmensBernsteinBrodskyBloodVolume() {
+        return this.weight * (70 / Math.sqrt(this.getBMI() / 22));
+    }
+
+    getWeightBasedBloodVolume() {
+        return (this.gender == "male" ? 70 : 65) * this.weight;
+    }
+
+    getBMI() {
+        return this.weight / Math.pow(this.height / 100, 2);
+    }
+
+    getBACInGramPct(alcohol) {
+        return (alcohol.content * 100) / this.getBloodVolume();
+    }
+}
+
+export class Alcohol {
+    constructor(content) {
+        this.content = content; // in mg
+    }
+
+    static fromPercentageAndVolume(percentage, volume) {
+        return new Alcohol((volume * percentage) / 100 / VOL2WEIGHT);
+    }
+
+    static fromStandardDrink(standard_drink) {
+        return new Alcohol(standard_drink * 10);
+    }
+}
+
+export class Drink {
+    constructor(start_date, duration, alcohol) {
+        this.start_date = start_date;
+        this.duration = duration;
+        this.alcohol = alcohol;
+    }
+
+    getAlcoholContentForDate(person, date) {
+        /* https://www.intmath.com/blog/mathematics/math-of-drugs-and-bodies-pharmacokinetics-4098 */
+        // 1/V(AFDe(-At) - EVC)
+
+        /* https://en.wikipedia.org/wiki/Alcohol_(drug)#:~:text=The%20oral%20bioavailability%20of%20ethanol,the%20dose%20of%20ethanol%20administered. */
+        /* https://www.researchgate.net/publication/333219749_Alcohol_its_absorption_distribution_metabolism_and_excretion_in_the_body_and_pharmacokinetic_calculations */
+
+        return Math.random() * 10;
+    }
+}
+
+export class Timeline {
+    constructor() {
+        this.data = [];
+    }
+
+    addDrink(drink) {
+        this.data.push(drink);
+    }
+}
